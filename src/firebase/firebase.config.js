cbf1b3bba6c115c7aebedf622d822374d88044a7
JubaYer0307 <jubayerahmed0307@gmail.com>
2023-06-12 18:10:59 +0600

    firebase setup

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBEK79w01sfm0bJKL_Lo8kBXoWr0O2pzI8",
+  authDomain: "photome-a5c0d.firebaseapp.com",
+  projectId: "photome-a5c0d",
+  storageBucket: "photome-a5c0d.appspot.com",
+  messagingSenderId: "715378159027",
+  appId: "1:715378159027:web:a0d58e364ce32f7b46afc6"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
