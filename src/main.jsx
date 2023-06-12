ba0d6a21c24944575977a5db916470face3d394b
JubaYer0307 <jubayerahmed0307@gmail.com>
2023-06-12 18:33:40 +0600

    authentication

--- src/main.jsx
+++ src/main.jsx
@@ -7,13 +7,16 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import { router } from './Routes/Router';
+import AuthProvider from './provider/AuthProvider';
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <AuthProvider>
     <div className='max-w-screen-xl mx-auto'>
     <RouterProvider router={router} />
     </div>
+    </AuthProvider>
   </React.StrictMode>,
 )
