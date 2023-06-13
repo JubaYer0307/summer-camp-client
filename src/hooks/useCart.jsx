c67123a4aeff55b18ae03346f95a8b33ed8a1a61
JubaYer0307 <jubayerahmed0307@gmail.com>
2023-06-14 00:04:31 +0600

    jwt token

--- src/hooks/useCart.jsx
+++ src/hooks/useCart.jsx
@@ -5,12 +5,15 @@ import { useContext } from 'react';
 const useCart = () => {
 
     const {user} = useContext(AuthContext);
+    const token = localStorage.getItem('access-token');
 
 
     const {  refetch, data: cart = []} = useQuery({
         queryKey: ['selectedClass', user?.email],
         queryFn: async () => {
-            const res = await fetch(`http://localhost:5000/selectedClass?email=${user.email}`)
+            const res = await fetch(`http://localhost:5000/selectedClass?email=${user.email}`, { headers: {
+                authorization: `bearer ${token}`
+            }})
             return res.json();
         },
       })
